8a9ef7999aa688defa435b198bab50bc49dcced8
Nestor Hugo <nestorklauck@gmail.com>
2023-11-29 21:50:06 -0300

    Remove unnecessary code and update header element

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Nestorzinho!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header className="App-header">
+          <div className="logo"></div>
+        </header>
       </div>
     );
   }
