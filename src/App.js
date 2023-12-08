5209c4e1e14067e37b5f5c19b68682c76cb6efaa
Nestor Hugo <nestorklauck@gmail.com>
2023-12-07 23:29:35 -0300

    Add logo to header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+import logo from "./img/logo.svg";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <div className="logo"></div>
+          <div className="logo">
+            <img src={logo} alt="logo" />
+            <p>
+              <strong>b8ks</strong>
+            </p>
+          </div>
         </header>
       </div>
     );
