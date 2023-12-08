e823db2148f5cf40dcfece20f0c7d576e1b831cb
Nestor Hugo <nestorklauck@gmail.com>
2023-12-08 00:23:48 -0300

    Refactor App and Logo components

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import logo from "../../img/logo.svg";
+import "./style.css";
 
 function Logo() {
   return (
     <div className="logo">
-      <img src={logo} alt="logo"></img>
+      <img src={logo} alt="logo" className="logo-img"></img>
       <p>
         <strong>b8</strong>ks
       </p>
