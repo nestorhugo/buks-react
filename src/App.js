2153d69e02700b3fc6d8c934e03279c25f296f09
Nestor Hugo <nestorklauck@gmail.com>
2023-12-09 23:01:15 -0300

    Remove App.css to start using styled-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
-import "./App.css";
 import Header from "./components/Header";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(to right, #f3f3f3 0%, #e6e6e6 100%);
+  li {
+    list-style: none;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Header />
-    </div>
+    </AppContainer>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,14 +3,24 @@ import "./style.css";
 import Logo from "../../components/Logo";
 import OpcoesMenu from "../../components/opcoes-menu";
 import IconesHeader from "../../components/icones-header";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  background-color: #e9e9e9;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: rgb(48, 48, 48);
+  justify-content: center;
+`;
 
 function Header() {
   return (
-    <header className="App-header">
+    <HeaderContainer>
       <Logo />
       <OpcoesMenu />
       <IconesHeader />
-    </header>
+    </HeaderContainer>
   );
 }
 
