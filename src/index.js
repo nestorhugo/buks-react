1e6a7049231c4cfa6f0a0a930bf91f5da1c337dd
nestorhugo <nestorhugo@proton.me>
2024-01-10 20:10:44 -0300

    Add react-router-dom and update dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
+import { createGlobalStyle } from "styled-components";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const GlobalStyle = createGlobalStyle`
+body {
+    margin: 0;
+    padding: 0;
+    font-family: "Archivo", sans-serif;
+  }
+  
+  li {
+    list-style: none;
+  }
+`;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>
+);
